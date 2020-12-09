6ff21df6aa45be336aabb330c8f183ea733a63c8
boconnell2 <bmoconnell15@gmail.com>
2020-12-09 16:56:12 -0500

    commit tree f33b97616f1122c4adf664d6b569f35ae43723ab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'val';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not val';
+}
